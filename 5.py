689bf670da56715fdc648e40684c13d718f4d9a7
Jaehoon Youn <82512449+jay-0625@users.noreply.github.com>
2021-05-11 19:50:35 +0900

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,10 @@
+score=[95.5, 88.0, 82.6, 80.0]
+#국어-수학-영어-과학
+
+#score[1]=90.0
+#score[2]=80.5
+
+score[1:3]=[90.0, 80.5]
+
+print ("최종점수: ", score[1], score[2])
+print ("평균점수: ", sum(score)/len(score))
