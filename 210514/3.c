fdf63d94748c950f66684ae8538cf7b1b95327eb
Jaehoon Youn <82512449+jay-0625@users.noreply.github.com>
2021-05-18 19:58:11 +0900

    Add files via upload

---
+++ 210514/3.c
@@ -0,0 +1,15 @@
+#include studio.h;
+
+int usrValue;
+
+main(){
+    printf("Enter your number to calculate");
+    usrValue=scanf();
+    if((int(usrValue)-(3)usrValue)==0){
+        printf("This number is a multiple of 3");
+    }
+    else{
+        printf("This number is not a multiple of 3");
+    }
+}
+return 0;
---
+++ 210514/3.py
@@ -0,0 +1,6 @@
+print("Enter your number to calculate")
+usrValue=input()
+if(int(usrValue)%3==1):
+    print("This number is a multiple of 3")
+else:
+    print("This number is not a multiple of 3")
